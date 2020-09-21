57df78557ac6b9dd8ae568e9f55ee97c736a6828
evinracher <eevinley@gmail.com>
2020-09-21 18:40:32 -0500

    Updates

---
+++ precache-manifest.5092c1c87ccaa2f30f21fe399b0ee317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8a2ef4ee736fb7ce9f0e5217a4f680",
+    "url": "/pokemon-react-app/index.html"
+  },
+  {
+    "revision": "f1872f85f14111b0619f",
+    "url": "/pokemon-react-app/static/css/main.228ad90f.chunk.css"
+  },
+  {
+    "revision": "0d9a2485fa075fa8cc21",
+    "url": "/pokemon-react-app/static/js/2.1f8b7d37.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/pokemon-react-app/static/js/2.1f8b7d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1872f85f14111b0619f",
+    "url": "/pokemon-react-app/static/js/main.455d7d18.chunk.js"
+  },
+  {
+    "revision": "b8e8594d946e04b30f7e",
+    "url": "/pokemon-react-app/static/js/runtime-main.5f339910.js"
+  },
+  {
+    "revision": "50ab3c9fdfba9f167f872cadfb9df993",
+    "url": "/pokemon-react-app/static/media/whos_that_pokemon.50ab3c9f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87eb87f17e74643888cb3672e65b677d.js"
+  "/pokemon-react-app/precache-manifest.5092c1c87ccaa2f30f21fe399b0ee317.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokemon-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
